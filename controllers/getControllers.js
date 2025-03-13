9f5b37dd3f9760b583b551f4160e830614428f2d
Morlintie <ahmetyasintoraman@gmail.com>
2025-03-13 17:37:49 +0300

    Adding the error handling by manually

--- controllers/getControllers.js
+++ controllers/getControllers.js
@@ -1,67 +1,71 @@
 const Product = require("../models/products.js");
 
 const getAll = async (req, res) => {
-  const { name, company, featured, sort, fields, numericFilters } = req.query;
-  const queryObject = {};
-  if (name) {
-    queryObject.name = {
-      $regex: name,
-      $options: "i",
-    };
+  try {
+    const { name, company, featured, sort, fields, numericFilters } = req.query;
+    const queryObject = {};
+    if (name) {
+      queryObject.name = {
+        $regex: name,
+        $options: "i",
+      };
+    }
+    if (company) {
+      queryObject.company = company;
+    }
+    if (featured) {
+      queryObject.featured = featured === "true" ? true : false;
+    }
+    if (numericFilters) {
+      //price>40,rating<=2
+      const operatorTransformer = {
+        ">": "$gt",
+        ">=": "$gte",
+        "<": "$lt",
+        "<=": "$lte",
+        "=": "$eq",
+      };
+
+      const regex = /\b(>|>=|<|<=|=)\b/g;
+      const mNumericFilters = numericFilters.replace(regex, (match) => {
+        return `-${operatorTransformer[match]}-`;
+      });
+      const separateFilter = mNumericFilters.split(",");
+      const checker = ["price", "rating"];
+      separateFilter.forEach((filter) => {
+        const [field, operator, value] = filter.split("-");
+        if (checker.includes(field)) {
+          queryObject[field] = {
+            [operator]: value,
+          };
+        }
+      });
+    }
+
+    console.log(queryObject);
+
+    let result = Product.find(queryObject);
+
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result.sort(sortList);
+    }
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result.select(fieldsList);
+    }
+
+    const limit = req.query.limit || 10;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
+    result.limit(limit).skip(skip);
+
+    const products = await result;
+
+    res.status(200).json({ nmHits: products.length, products: products });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err });
   }
-  if (company) {
-    queryObject.company = company;
-  }
-  if (featured) {
-    queryObject.featured = featured === "true" ? true : false;
-  }
-  if (numericFilters) {
-    //price>40,rating<=2
-    const operatorTransformer = {
-      ">": "$gt",
-      ">=": "$gte",
-      "<": "$lt",
-      "<=": "$lte",
-      "=": "$eq",
-    };
-
-    const regex = /\b(>|>=|<|<=|=)\b/g;
-    const mNumericFilters = numericFilters.replace(regex, (match) => {
-      return `-${operatorTransformer[match]}-`;
-    });
-    const separateFilter = mNumericFilters.split(",");
-    const checker = ["price", "rating"];
-    separateFilter.forEach((filter) => {
-      const [field, operator, value] = filter.split("-");
-      if (checker.includes(field)) {
-        queryObject[field] = {
-          [operator]: value,
-        };
-      }
-    });
-  }
-
-  console.log(queryObject);
-
-  let result = Product.find(queryObject);
-
-  if (sort) {
-    const sortList = sort.split(",").join(" ");
-    result.sort(sortList);
-  }
-  if (fields) {
-    const fieldsList = fields.split(",").join(" ");
-    result.select(fieldsList);
-  }
-
-  const limit = req.query.limit || 10;
-  const page = req.query.page || 1;
-  const skip = (page - 1) * limit;
-  result.limit(limit).skip(skip);
-
-  const products = await result;
-
-  res.status(200).json({ nmHits: products.length, products: products });
 };
 
 module.exports = {
